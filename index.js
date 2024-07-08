237061c05008678007cd5b6c112ef43371f42f55
Elad Levy <162992264+eladlevy42@users.noreply.github.com>
2024-07-08 16:12:13 +0300

    deploy working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const { verifyToken } = require("./middlewares/auth.middleware");
+const path = require("path");
 
 dotenv.config(); // Load config
 const app = express();
@@ -21,9 +22,14 @@ async function main() {
   app.use("/api/task", verifyToken, tasksRoutes);
   app.use("/api/auth", authRoutes);
   app.use("/api/user", verifyToken, userRoutes);
+  app.use(express.static("public"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-  app.use(express.static("public"));
 }
 main();
